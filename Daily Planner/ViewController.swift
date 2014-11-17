4b76ef24794777ac1c368c3d14a15295918d2c39
Ruyao Zhang <rika.zry@gmail.com>
2014-11-16 19:13:27 -0500

    Initial Commit

---
+++ Daily Planner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Daily Planner
+//
+//  Created by Rika on 11/16/14.
+//  Copyright (c) 2014 superz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
